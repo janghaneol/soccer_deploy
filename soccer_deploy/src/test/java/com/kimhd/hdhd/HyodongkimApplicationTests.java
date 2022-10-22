d9ffc650c11174e33c4b38e2be084ac6632617c5
hyodongkim <kimhd853@naver.com>
2022-10-22 16:55:51 +0900

    setting 1

---
+++ soccer_deploy/src/test/java/com/kimhd/hdhd/HyodongkimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kimhd.hdhd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyodongkimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
